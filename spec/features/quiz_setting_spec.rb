d095951763a8fcf58a499906b508342d037b3ee9
Mal Pinder <mal.pinder@fundingcircle.com>
2014-05-16 17:24:18 +0100

    Add bare-bones quiz setting spec.

---
+++ spec/features/quiz_setting_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature "Setting a quiz" do
+
+  scenario "with simple questions" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+
+    visit '/'
+    click_link 'Set new quiz'
+
+    fill_in :quiz_title, with: "My first quiz"
+    fill_in :quiz_instructions, with: "Answer all questions."
+
+    click_button "Create quiz"
+
+    expect(page).to have_content "Your quiz has been created."
+
+    click_link "Add a question"
+
+    expect(page).to have_content "Adding a question to 'My first quiz'"
+    fill_in :question_body, with: "What is my first question?"
+    fill_in :question_available_marks, with: "1"
+    click_button "Add question"
+
+    expect(page).to have_content "Question added"
+
+    visit '/'
+    expect(page).to have_content "My first quiz"
+    expect(page).to have_content "Unpublished"
+  end
+
+
+end
