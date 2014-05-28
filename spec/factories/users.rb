63a7ba5e58ad512cf01562d0c551a8b160989fa3
Mal Pinder <malone.pinder@gmail.com>
2014-05-28 16:23:52 +0100

    Update user factory to use token sequence.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 
 FactoryGirl.define do
+
+  sequence :token do |n|
+    "abc-#{n}"
+  end
+
   factory :user do
     sequence(:email) { |n| "user-#{n}@example.com" }
     sequence(:display_name) { |n| "User #{n}" }
-    session_token 'abc-123'
+    session_token { generate :token }
   end
 end
